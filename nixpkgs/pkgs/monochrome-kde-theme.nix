2842e5dc2f556e106434d9d43e0868a27e2a821b
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2024-05-12 18:34:41 +0200

    Fixed issue with the GTK theme Generation 6 Date: 2024-05-12 18:34:38 System: 24.05.20240511.062ca2a Kernel: 6.8.9-cachyos

--- nixpkgs/pkgs/monochrome-kde-theme.nix
+++ nixpkgs/pkgs/monochrome-kde-theme.nix
@@ -9,11 +9,12 @@ pkgs.stdenv.mkDerivation {
     sha256 = "1nsffmp0z5a93pwx1jalaiidqrjby9hc1llkxdph9k67cd1i0wcs";
   };
   installPhase = ''
-    mkdir -p $out/share/
-    cp -R ./aurorae/ $out/share/
-    cp -R ./color-schemes/ $out/share/
-    cp -R ./konsole/ $out/share/
-    cp -R ./plasma/ $out/share/
-    cp -R ./gtk/Monochrome/ $out/share/themes/
+    mkdir -p $out/share/{aurorae,color-schemes,konsole,plasma,themes}
+    mkdir $out/share/themes/Monochrome
+    cp -r ./aurorae/. $out/share/aurorae
+    cp -r ./color-schemes/. $out/share/color-schemes
+    cp -r ./konsole/. $out/share/konsole
+    cp -r ./plasma/. $out/share/plasma
+    cp -r ./gtk/Monochrome/. $out/share/themes/Monochrome
   '';
 }
