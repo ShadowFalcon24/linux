fd49bfb8ed2fbcab264c8f0a3dc9afc24e8e3ef6
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2024-05-15 15:53:06 +0200

    Changed how systemd-boot detects the target resolution Generation 6 Date: 2024-05-15 15:53:04 System: 24.05.20240512.2057814 Kernel: 6.9.0-cachyos

--- base/boot.nix
+++ base/boot.nix
@@ -13,6 +13,10 @@
   # chaotic.scx.scheduler = "scx_rusty";
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    consoleMode = "auto";
+    netbootxyz.enable = true;
+  };
 }
