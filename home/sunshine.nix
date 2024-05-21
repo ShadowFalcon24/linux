1170393fe9c9f5cecbd9ca4fc0c0304f5629e279
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2024-05-21 19:28:51 +0200

    Added delay before starting sunshine Generation 4 Date: 2024-05-21 19:28:48 System: 24.05.20240519.6c0b7a9 Kernel: 6.9.1-cachyos

--- home/sunshine.nix
+++ home/sunshine.nix
@@ -16,6 +16,7 @@ in {
       systemd.user.services = {
         sunshine = {
           Unit.Description = "Sunshine is a Game stream host for Moonlight.";
+          Service.ExecStartPre = "${pkgs.coreutils}/bin/sleep 10";
           Service.ExecStart = "${pkgs.sunshine}/bin/sunshine";
           Install.WantedBy = ["graphical-session.target"];
         };
