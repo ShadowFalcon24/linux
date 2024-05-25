874f4312a451de2463e4864922ea2d2f85c4939c
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2024-05-25 17:00:20 +0200

    Enabled steam and protonup to test NVK Generation 4 Date: 2024-05-25 17:00:14 System: 24.05.20240521.5710852 Kernel: 6.9.1-cachyos

--- software/gaming.nix
+++ software/gaming.nix
@@ -20,10 +20,8 @@ in {
           mangohud # Performance Overlay
 
           # Wine and bottles
-          # protonup
-          # wineWowPackages.stable
-          # winetricks
-          # bottles
+          protonup
+          bottles
 
           # Minecraft
           temurin-bin
@@ -31,13 +29,13 @@ in {
         ];
 
         # Install steam and other fancy things
-        # programs.steam.enable = true;
+        programs.steam.enable = true;
         programs.gamemode.enable = true;
 
         # Set protonup install folder
-        # environment.sessionVariables = {
-        #   STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\\\${HOME}/.steam/root/compatibilitytools.d";
-        # };
+        environment.sessionVariables = {
+          STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\\\${HOME}/.steam/root/compatibilitytools.d";
+        };
       }
     )
     (lib.mkIf cfg.sunshine.enable {
