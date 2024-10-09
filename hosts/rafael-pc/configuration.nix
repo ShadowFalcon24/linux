3eda2f1a4ab21110f95cd2c382fc2b090dab4b77
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2024-10-09 21:40:04 +0200

    fix: turn nvidia off

--- hosts/rafael-pc/configuration.nix
+++ hosts/rafael-pc/configuration.nix
@@ -23,7 +23,7 @@
   displayManager.sddm.enable = true;
 
   # Install nvidia drivers
-  graphics.driver.enableNvidia = true; # Disable for AMD
+  graphics.driver.enableNvidia = false; # Disable for AMD
 
   # Software
   software.gaming.enable = true; # Enable steam und lutris
