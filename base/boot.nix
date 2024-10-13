2c21028b2628f79bb8ce2a78e2903de6deb0d4ef
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2024-10-13 19:27:59 +0200

    Switch to ZEN kernel Generation 14 Date: 2024-10-13 19:27:57 System: 24.11.20241009.5633bcf Kernel: 6.10.9-zen1

--- base/boot.nix
+++ base/boot.nix
@@ -4,12 +4,12 @@
   pkgs,
   ...
 }: {
-  # OLD: Change kernel to the zen kernel
-  #boot.kernelPackages = pkgs.linuxPackages_zen;
+  # Change kernel to the zen kernel
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   # Change kernel to the CachyOS kernel
-  boot.kernelPackages = pkgs.linuxPackages_cachyos;
-  chaotic.scx.enable = true; # by default uses scx_rustland scheduler
+  #boot.kernelPackages = pkgs.linuxPackages_cachyos;
+  #chaotic.scx.enable = true; # by default uses scx_rustland scheduler
   # chaotic.scx.scheduler = "scx_rusty";
 
   # Bootloader.
