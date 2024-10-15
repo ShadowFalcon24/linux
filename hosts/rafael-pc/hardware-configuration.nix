0e1b155254abfdc7f43c430d027e69beef8b98fd
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2024-10-15 20:41:41 +0200

    fix(disks): fix disk uuids Generation 3 Date: 2024-10-15 20:41:35 System: 24.11.20241014.a3c0b3b Kernel: 6.10.9-zen1

--- hosts/rafael-pc/hardware-configuration.nix
+++ hosts/rafael-pc/hardware-configuration.nix
@@ -18,31 +18,31 @@
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/d93a33f5-572a-4210-ba3d-d7b351043033";
+    device = "/dev/disk/by-uuid/f10bfe37-12d9-4bc1-b4c4-ecc0fd0c9346";
     fsType = "btrfs";
     options = ["compress=zstd" "subvol=root"];
   };
 
   fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/d93a33f5-572a-4210-ba3d-d7b351043033";
+    device = "/dev/disk/by-uuid/f10bfe37-12d9-4bc1-b4c4-ecc0fd0c9346";
     fsType = "btrfs";
     options = ["compress=zstd" "subvol=home"];
   };
 
   fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/d93a33f5-572a-4210-ba3d-d7b351043033";
+    device = "/dev/disk/by-uuid/f10bfe37-12d9-4bc1-b4c4-ecc0fd0c9346";
     fsType = "btrfs";
     options = ["compress=zstd" "noatime" "subvol=nix"];
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/56D1-73F2";
+    device = "/dev/disk/by-uuid/A09B-3045";
     fsType = "vfat";
     options = ["fmask=0022" "dmask=0022"];
   };
 
   swapDevices = [
-    {device = "/dev/disk/by-uuid/9f7d6577-6007-4dbd-a27e-584abbbc25be";}
+    {device = "/dev/disk/by-uuid/54c59400-3a61-476d-a678-1a995311f3d3";}
   ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
--- hosts/rafael-pc/host.nix
+++ hosts/rafael-pc/host.nix
@@ -23,31 +23,36 @@
   boot.supportedFilesystems = ["ntfs"];
 
   # SSD
-  fileSystems."/mnt/ssd/virtualization" = {
-    device = "/dev/disk/by-uuid/2e8f5bb1-528f-4a80-9e0a-7769f1e98968";
-    fsType = "btrfs";
-    options = ["compress=zstd" "subvol=virtualization"];
-  };
-  fileSystems."/mnt/ssd/games" = {
-    device = "/dev/disk/by-uuid/2e8f5bb1-528f-4a80-9e0a-7769f1e98968";
-    fsType = "btrfs";
-    options = ["compress=zstd" "subvol=games"];
-  };
+  #fileSystems."/mnt/ssd/virtualization" = {
+  #  device = "/dev/disk/by-uuid/2e8f5bb1-528f-4a80-9e0a-7769f1e98968";
+  #  fsType = "btrfs";
+  #  options = ["compress=zstd" "subvol=virtualization"];
+  #};
+  #fileSystems."/mnt/ssd/games" = {
+  #  device = "/dev/disk/by-uuid/2e8f5bb1-528f-4a80-9e0a-7769f1e98968";
+  #  fsType = "btrfs";
+  #  options = ["compress=zstd" "subvol=games"];
+  #};
 
   # HDD
   fileSystems."/mnt/hdd/development" = {
-    device = "/dev/disk/by-uuid/9cfca145-d357-427b-b247-63df275332c5";
+    device = "/dev/disk/by-uuid/4c1a2789-da2b-4425-b80a-b7e6aa0194ce";
     fsType = "btrfs";
     options = ["compress=zstd" "subvol=development"];
   };
   fileSystems."/mnt/hdd/backups" = {
-    device = "/dev/disk/by-uuid/9cfca145-d357-427b-b247-63df275332c5";
+    device = "/dev/disk/by-uuid/4c1a2789-da2b-4425-b80a-b7e6aa0194ce";
     fsType = "btrfs";
     options = ["compress=zstd" "subvol=backups"];
   };
   fileSystems."/mnt/hdd/school" = {
-    device = "/dev/disk/by-uuid/9cfca145-d357-427b-b247-63df275332c5";
+    device = "/dev/disk/by-uuid/4c1a2789-da2b-4425-b80a-b7e6aa0194ce";
     fsType = "btrfs";
     options = ["compress=zstd" "subvol=school"];
+  };
+  fileSystems."/mnt/hdd/games" = {
+    device = "/dev/disk/by-uuid/4c1a2789-da2b-4425-b80a-b7e6aa0194ce";
+    fsType = "btrfs";
+    options = ["compress=zstd" "subvol=games"];
   };
 }
