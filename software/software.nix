91bef1f528d1795d2271bb064b6343a80518d335
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2024-12-08 17:21:38 +0100

    feat(discord): switch to using discord-canary Generation 27 Date: 2024-12-08 17:21:32 System: 25.05.20241207.22c3f2c Kernel: 6.12.2-zen1

--- software/software.nix
+++ software/software.nix
@@ -38,7 +38,7 @@
     # ciscoPacketTracer8
 
     # Chat clients
-    vesktop # Discord but better
+    discord-canary # Discord but better
     # Matrix Client: element-desktop
 
     # E-Mail clients
