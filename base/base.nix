744e44debce386b7c0a49560476e817a98230b7b
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2025-05-01 02:58:21 +0200

    feat: Enable bluetooth Generation 114 Date: 2025-05-01 02:58:17 System: 25.05.20250423.8a2f738 Kernel: 6.14.3-zen1

--- base/base.nix
+++ base/base.nix
@@ -30,6 +30,9 @@
     enable32Bit = true;
   };
 
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
